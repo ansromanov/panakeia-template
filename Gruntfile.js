695e7630355e39987b7e11b5aab8a2c73483d009
Andrey Romanov <ansromanov@gmail.com>
2017-03-24 21:53:20 +0300

    a lot of upgrades to css processing

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function (grunt) {
           expand: true,
           cwd: 'Development/templates/scss',
           src: '*.scss',
-          dest: 'Development/css',
+          dest: 'Development/templates/postcss',
           ext: '.css'
         }]
       },
@@ -23,61 +23,134 @@ module.exports = function (grunt) {
         },
         files: [{
           expand: true,
-          cwd: 'Development/templates/scss',
+          cwd: 'Production/templates/scss',
           src: '*.scss',
-          dest: 'Development/css',
+          dest: 'Production/templates/postcss',
           ext: '.css'
         }]
       }
     },
     pug: {
-      compile: {
+      dev: {
         options: {
           client: false,
           pretty: true
         },
         files: [{
-          cwd: "Development/templates/",
+          cwd: "Development/templates/pug",
           src: "**/*.pug",
           dest: "Development/",
+          expand: true,
+          ext: ".html"
+        }]
+      },
+      prod: {
+        options: {
+          client: false,
+          pretty: true
+        },
+        files: [{
+          cwd: "Production/templates/pug",
+          src: "**/*.pug",
+          dest: "Production/",
           expand: true,
           ext: ".html"
         }]
       }
     },
     imagemin: {
-      dynamic: {
+      dev: {
         files: [{
           expand: true,
           cwd: 'Development/templates/images/',
           src: ['**/*.{png,jpg,gif}'],
           dest: 'Development/images/'
         }]
+      },
+      prod: {
+        files: [{
+          expand: true,
+          cwd: 'Production/templates/images/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'Production/images/'
+        }]
+      }
+    },
+    postcss: {
+      dev: {
+        options: {
+          map: {
+            inline: false,
+            annotation: 'Development/templates/postcss'
+          },
+          processors: [
+            require('autoprefixer')(),
+            require('stylelint')()
+          ],
+        },
+        dist: {
+          src: 'Development/templates/postcss/style.css',
+          dest: 'Development/css/style.css',
+        }
+      },
+      prod: {
+        options: {
+          map: {
+            inline: false,
+            annotation: 'Development/templates/postcss'
+          },
+          processors: [
+            require('autoprefixer')()
+          ],
+        },
+        dist: {
+          src: 'Development/templates/postcss/style.css',
+          dest: 'Development/css/style.css',
+        }
       }
     },
     watch: {
-      sass: {
+      sass_dev: {
         files: ['Development/templates/scss/*.{scss,sass}'],
-        tasks: ['sass:dev']
+        tasks: ['sass:dev'],
       },
-      pug: {
-        files: ['Development//templates/*.pug'],
-        tasks: ['pug:compile']
+      sass_prod: {
+        files: ['Production/templates/scss/*.{scss,sass}'],
+        tasks: ['sass:prod'],
       },
-      imagemin: {
+      pug_dev: {
+        files: ['Development/templates/pug/*.pug'],
+        tasks: ['pug:dev'],
+      },
+      pug_prod: {
+        files: ['Production/templates/pug/*.pug'],
+        tasks: ['pug:prod'],
+      },
+      imagemin_dev: {
         files: ['Development/templates/images/*.{png,jpg,gif}'],
-        tasks: ['imagemin:dynamic']
-      }
+        tasks: ['imagemin:dev']
+      },
+      imagemin_prod: {
+        files: ['Production/templates/images/*.{png,jpg,gif}'],
+        tasks: ['imagemin:prod']
+      },
+      postcss_dev: {
+        files: ['Development/templates/postcss/style.css'],
+        tasks: ['postcss:dev']
+      },
+      postcss_prod: {
+        files: ['Production/templates/postcss/style.css'],
+        tasks: ['postcss:prod']
+      },
 
     }
-
-
   });
 
   grunt.loadNpmTasks('grunt-contrib-pug');
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-postcss');
 
   grunt.registerTask('default', [
     'watch'
